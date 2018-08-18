bf6e164f06ba3e4e784fd5d4c5fa2a857b38d60c
mao2009 <en>
2018-08-19 08:55:34 +0900

    add login method

--- addons/script.module.takoyaki/lib/takoyaki/__init__.py
+++ addons/script.module.takoyaki/lib/takoyaki/__init__.py
@@ -3,11 +3,13 @@
 import sys
 import urllib
 import urlparse
-
+import pickle
 import requests
 
 
 class Takoyaki(object):
+
+    USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 
     __base__url = sys.argv[0]
     __handle__ = int(sys.argv[1])
@@ -17,6 +19,23 @@ class Takoyaki(object):
 
     def run(self):
         self.select_mode()
+
+    @classmethod
+    def write_cookie(cls, session, cookie_file_name='cookie.dump'):
+        with open('cookie_file_name', 'wb') as fp:
+            pickle.dump(session.cookies, fp)
+
+    @classmethod
+    def read_cookie(cls, session, cookie_file_name='cookie.dump'):
+        with open(cookie_file_name, 'rb') as fp:
+            cookie = pickle.load(fp)
+            session.cookies.update(cookie)
+
+    @classmethod
+    def login(cls, login_url, query):
+        session = requests.Session()
+        response = session.post(login_url, data=query)
+        return session, response
 
     @classmethod
     def urljoin(cls, base, *urls):
@@ -34,7 +53,6 @@ class Takoyaki(object):
         params = urlparse.parse_qs(sys.argv[2][1:])
         return {key: value[0] for key, value in params.items()}
 
-    @classmethod
-    def download_html(cls, url):
-        headers = {'user-agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+    def download_html(self, url, cookie=None):
+        headers = {'user-agent': self.USER_AGENT}
         return requests.get(url, headers=headers).text
