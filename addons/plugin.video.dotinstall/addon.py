ab806fcf32c632d6c1b458258c624466f2f3149c
mao2009 <en>
2018-08-19 14:19:55 +0900

    mod login

--- addons/plugin.video.dotinstall/addon.py
+++ addons/plugin.video.dotinstall/addon.py
@@ -11,6 +11,7 @@ from takoyaki import Takoyaki
 class DotInstall(Takoyaki):
 
     BASE_URL = 'https://dotinstall.com/'
+    LOGIN_URL = 'https://dotinstall.com/login.php'
 
     def select_mode(self):
         modes = {
@@ -21,8 +22,20 @@ class DotInstall(Takoyaki):
         }
         super(DotInstall, self).select_mode(modes)
 
+    def login(self):
+        url = 'https://dotinstall.com/login'
+        html = self.download_html(url)
+        soup = BeautifulSoup(html)
+        token = soup.find('input', class_='one_time_token').get('value')
+        query = {
+            'mail': self.username,
+            'password': self.password,
+            'token': token
+        }
+        super(DotInstall, self).login(self.LOGIN_URL, query, mode='post')
+
     def lessons(self):
-        url = self.urljoin(self.BASE_URL, 'lessons')
+        url = self.url_join(self.BASE_URL, 'lessons')
         html = self.download_html(url)
         bs = BeautifulSoup(html)
         elements = bs.find_all(class_='span8')
@@ -32,7 +45,7 @@ class DotInstall(Takoyaki):
 
         img_url = self.params.get('img_url')
         for lesson in lessons:
-            link = self.urljoin(self.BASE_URL, lesson.get('href'))
+            link = self.url_join(self.BASE_URL, lesson.get('href'))
             title = lesson.text
             li = xbmcgui.ListItem(title, iconImage=img_url)
             query = {'mode': 'lesson', 'link': link, 'img_url': img_url}
@@ -47,7 +60,7 @@ class DotInstall(Takoyaki):
         lessons = bs.find(id='lessons_list').find_all('a')
         img_url = self.params.get('img_url')
         for lesson in lessons:
-            link = self.urljoin(self.BASE_URL, lesson.get('href'))
+            link = self.url_join(self.BASE_URL, lesson.get('href'))
             title = lesson.text
             li = xbmcgui.ListItem(title, iconImage=img_url)
             query = {'mode': 'select_source', 'link': link, 'img_url': img_url}
@@ -62,7 +75,7 @@ class DotInstall(Takoyaki):
         sources = bs.find_all('source')
         img_url = self.params.get('img_url')
         for source in sources:
-            link = self.urljoin(self.BASE_URL, source.get('src'))
+            link = self.url_join(self.BASE_URL, source.get('src'))
             title = source.get('data-res') + 'P ' + source.get('type')
             li = xbmcgui.ListItem(title, iconImage=img_url)
             query = {'mode': 'play_video', 'link': link, 'img_url': img_url}
@@ -72,9 +85,12 @@ class DotInstall(Takoyaki):
 
     def top_menu(self):
 
-        url = self.urljoin(self.BASE_URL, 'lessons')
+        url = self.url_join(self.BASE_URL, 'lessons')
         html = self.download_html(url)
         bs = BeautifulSoup(html)
+        if bs.find('i', class_='icon-user') is not None:
+            self.login()
+
         elements = bs.find_all(class_='span8')
 
         for element_num, element in enumerate(elements):
@@ -86,7 +102,7 @@ class DotInstall(Takoyaki):
                 continue
             img_url = image_element.get('src')
             title = title_element.text.encode('utf-8').replace('\n', '')
-            img_url = self.urljoin(self.BASE_URL, img_url)
+            img_url = self.url_join(self.BASE_URL, img_url)
             li = xbmcgui.ListItem(title, iconImage=img_url)
             query = {'mode': 'lessons', 'img_url': img_url, 'element_num': element_num}
             url = self.build_url(query)
--- addons/script.module.takoyaki/lib/takoyaki/__init__.py
+++ addons/script.module.takoyaki/lib/takoyaki/__init__.py
@@ -7,6 +7,7 @@ import urlparse
 import pickle
 import requests
 
+import xbmc
 import xbmcaddon
 
 
@@ -18,10 +19,27 @@ class Takoyaki(object):
     __handle__ = int(sys.argv[1])
     __addon__ = xbmcaddon.Addon()
     __addon_id__ = __addon__.getAddonInfo('id')
-    __addon_user_data_ = os.path.join('special://userdata/addon_data', __addon_id__)
 
     def __init__(self):
+
+        self.__addon_user_data__ = xbmc.translatePath(self.path_join('special://userdata/addon_data', self.__addon_id__))
         self.params = self.parse_parameter()
+        self.is_login = self.__addon__.getSetting('login')
+        if self.is_login:
+            self.password = self.__addon__.getSetting('password')
+            self.username = self.__addon__.getSetting('username')
+        self.cookies = self.read_cookie()
+        self.session = self.open_session()
+
+    def set_basic_auth(self, user, password):
+        self.session.auth = (user, password)
+
+    def open_session(self):
+        session = requests.Session()
+        session.cookies.update(self.cookies)
+        headers = {'user_agent': self.USER_AGENT}
+        session.headers.update(headers)
+        return session
 
     def run(self):
         self.select_mode()
@@ -32,38 +50,36 @@ class Takoyaki(object):
         selected_mode()
 
     def write_cookie(self, cookie, cookie_file_name='cookie.dump'):
-        cookie_file_path = os.path.join(self.__user_data_path__, cookie_file_name)
+        cookie_file_path = self.path_join(self.__addon_user_data__, cookie_file_name)
         with open(cookie_file_path, 'wb') as fp:
             pickle.dump(cookie, fp)
 
     def read_cookie(self, cookie_file_name='cookie.dump'):
-        cookie_file_path = os.path.join(self.__user_data_path__, cookie_file_name)
-        with open(cookie_file_path, 'rb') as fp:
-            cookie = pickle.load(fp)
-        return cookie
-
-    def login(self, login_url, query, mode='post', session=None, headers={}):
-        if session is None:
-            headers['user_agent'] = self.USER_AGENT
-            if mode == 'get' or mode == 'g':
-                cookies = requests.get(login_url, params=query, headers=headers).cookies
-
-            elif mode == 'post' or mode == 'p':
-                cookies = requests.post(login_url, data=query, headers=headers).cookies
-            else:
-                raise ValueError('Unexpected mode')
+        cookie_file_path = self.path_join(self.__addon_user_data__, cookie_file_name)
+        if os.path.exists(cookie_file_path):
+            with open(cookie_file_path, 'rb') as fp:
+                cookie = pickle.load(fp)
+            return cookie
         else:
-            if mode == 'get' or mode == 'g':
-                cookies = session.get(login_url, headers=headers, params=query).cookies
-            elif mode == 'post' or mode == 'p':
-                cookies = session.post(login_url, headers=headers, data=query).cookies
-            else:
-                raise ValueError('Unexpected mode')
+            return {}
+
+    def login(self, login_url, query, mode='post'):
+
+        if mode == 'get' or mode == 'g':
+            cookies = self.session.get(login_url,  params=query).cookies
+        elif mode == 'post' or mode == 'p':
+            cookies = self.session.post(login_url,  data=query).cookies
+        else:
+            raise ValueError('Unexpected mode')
+        self.session.cookies.update(cookies)
         self.write_cookie(cookie=cookies)
 
+    @classmethod
+    def path_join(cls, path, *paths):
+        return os.path.join(path, *paths).replace('\\', '/')
 
     @classmethod
-    def urljoin(cls, base, *urls):
+    def url_join(cls, base, *urls):
         jointed_url = base
         for url in urls:
             jointed_url = urlparse.urljoin(jointed_url,url)
@@ -78,17 +94,11 @@ class Takoyaki(object):
         params = urlparse.parse_qs(sys.argv[2][1:])
         return {key: value[0] for key, value in params.items()}
 
-    def download_html(self, url, mode='get', session=None, headers={}, query={}, cookies={}):
-        if session is None:
-            headers['user_agent'] = self.USER_AGENT
-            if mode == 'get' or mode == 'g':
-                return requests.get(url, headers=headers, params=query, cookies=cookies).text
-            elif mode == 'post' or mode == 'p':
-                return requests.post(url, headers=headers, data=query, cookies=cookies).text
-        else:
-            if mode == 'get' or mode == 'g':
-                return session.get(url, headers=headers, params=query, cookies=cookies).text
-            elif mode == 'post' or mode == 'p':
-                return session.post(url, headers=headers, data=query, cookies=cookies).text
+    def download_html(self, url, mode='get', query={}):
+
+        if mode == 'get' or mode == 'g':
+            return self.session.get(url, params=query).text
+        elif mode == 'post' or mode == 'p':
+            return self.session.post(url, data=query).text
 
         raise ValueError('Unexpected mode')
